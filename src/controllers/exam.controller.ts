87e8918847aeec67a95ff7205dc4b071a29bad6b
JimMartin1810 <martinosdimitrios@gmail.com>
2024-06-17 01:28:32 +0300

    some extra weird stuff (feet pics don't look)

--- src/controllers/exam.controller.ts
+++ src/controllers/exam.controller.ts
@@ -6,23 +6,6 @@ import { IUserInfoRequest } from "../models/user_info_request";
 import { IGeneralBloodRequest } from "../models/general_blood_request";
 import { IHormonalBloodRequest } from "../models/hormonal_blood_request";
 
-export const returnDate = async (req: Request, res: Response) => {
-  try {
-    const dateString = "20240615204225";
-    const year = parseInt(dateString.slice(0, 4), 10);
-    const month = parseInt(dateString.slice(4, 6), 10) - 1;
-    const day = parseInt(dateString.slice(6, 8), 10);
-    const hour = parseInt(dateString.slice(8, 10), 10);
-    const minute = parseInt(dateString.slice(10, 12), 10);
-    const second = parseInt(dateString.slice(12, 14), 10);
-    const date = new Date(year, month, day, hour, minute, second);
-    res.json(date.getTime()).status(200);
-  } catch (error) {
-    console.log(error);
-    res.send("Internal Server Error").status(500);
-  }
-};
-
 export const createBiochemicalBloodExam = async (
   req: Request,
   res: Response
